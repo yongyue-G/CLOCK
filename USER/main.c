374d93ef96ee6b20a9598990082d1f5629193a80
yongyue-G <3290615583@qq.com>
2026-04-12 18:33:11 +0800

    FreeRTOS

---
+++ USER/main.c
@@ -0,0 +1,19 @@
+#include "stm32f4xx.h"
+#include "usart.h"
+#include "delay.h"
+
+//ALIENTEK 探索者STM32F407开发板 实验0
+//STM32F4工程模板-库函数版本
+//技术支持：www.openedv.com
+//淘宝店铺：http://eboard.taobao.com
+//广州市星翼电子科技有限公司  
+//作者：正点原子 @ALIENTEK
+
+int main(void)
+{
+	while(1)
+	{
+		
+	}
+	
+}
